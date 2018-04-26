c98ef37f5d8b56cbb5882a442c19708b7145372b
Mjoshi4 <38687317+Mjoshi4@users.noreply.github.com>
2018-04-26 19:34:46 +0530

    Create Limit.cpp

---
+++ Interface/Limit.cpp
@@ -0,0 +1,15 @@
+# include <cstdlib>
+# include <iostream>
+# include <ctime>
+# include <limits>
+
+using namespace std;
+
+int main ( int argc, char *argv[] );
+void test01 ( );
+void test02 ( );
+void test03 ( );
+void test04 ( );
+void test05 ( );
+void test06 ( );
+void timestamp ( );
